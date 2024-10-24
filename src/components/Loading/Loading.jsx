42e5a20c37b2c09f0ec194fb216e8a6bbf1991df
chirag-08x <cagrawal081200@gmail.com>
2024-10-24 21:03:47 +0530

    footer completed

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,7 +2,14 @@ import { Box, CircularProgress } from "@mui/material";
 
 const Loading = () => {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        py: 4,
+      }}
+    >
       <CircularProgress />
     </Box>
   );
