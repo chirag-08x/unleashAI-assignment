88b2282f122f92d2cf66647a3155dbc2a501de0d
Chirag Agrawal <78968758+chirag-08x@users.noreply.github.com>
2024-10-25 02:56:36 +0530

    Pages/404 (#9)
    * added 404 page
    
    * minor fixes

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,11 +1,42 @@
 import React, { useEffect } from "react";
+import styled from "@emotion/styled";
+import { ErrorImg } from "../../assets";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 const Error = () => {
   useEffect(() => {
     document.title = "Page not found";
   }, []);
 
-  return <div>404!! Page not found </div>;
+  return (
+    <Wrapper>
+      <section className="section-center">
+        <Stack justifyContent={"center"} alignItems={"center"}>
+          <img style={{ maxWidthwidth: "500px" }} src={ErrorImg} alt="" />
+          <Link
+            to={"/"}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "0.5rem",
+              marginTop: "1rem",
+              color: "purple",
+              textDecoration: "underline",
+            }}
+          >
+            <FaArrowLeftLong /> Go Back
+          </Link>
+        </Stack>
+      </section>
+    </Wrapper>
+  );
 };
 
 export default Error;
+
+const Wrapper = styled.div`
+  min-height: 50vh;
+  padding: 3rem 0;
+`;
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 import HeaderLogo from "./Logo/header-logo.png";
+import ErrorImg from "./404/error.jpg";
 
-export { HeaderLogo };
+export { HeaderLogo, ErrorImg };
